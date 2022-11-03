332b12ba867d99662d8ac88af17196b1b929eeb4
MSalvador <salvador.systems@gmail.com>
2022-11-03 01:08:57 -0500

    FF

--- src/com/sanms/siso/eft/processor/ParametrosOperacion.java
+++ src/com/sanms/siso/eft/processor/ParametrosOperacion.java
@@ -6,11 +6,14 @@ package com.sanms.siso.eft.processor;
 
 import com.sanms.siso.eft.model.Stream;
 import com.sanms.siso.eft.utils.EnumErrores;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -120,25 +123,187 @@ public class ParametrosOperacion {
         return params;
     }
     
-    public String sysdate(String filename,String format, String nonusage0, String nonusage1, int pid){
-        String sysDate = new SimpleDateFormat(format).format(new Date());
-        return sysDate;
-    }
     
-    private boolean writeOnFile(String filename, String content) throws IOException{
-        boolean success = false;
-        
-        File fileNew = new File(filename);
-        fileNew.createNewFile();
-        
-        RandomAccessFile file = new RandomAccessFile(filename, "rw");
-        FileChannel fileChannel = file.getChannel();
-        
-        FileLock lock = null;
-        //lock = CheckFileLocked();
-        
-        return success;
+  public String sysdate(String filename, String format, String nonusage0, String nonusage1, int pid)
+  {
+    //logger.debug("ParametersSimulator.sysdate(): inicio");
+    String sysDate = new SimpleDateFormat(format).format(new Date());
+    writeOnFile(filename, sysDate);
+    //logger.debug("ParametersSimulator.sysdate(): fin");
+    return sysDate;
+  }
+
+  public String systime(String filename, String format, String nonusage0, String nonusage1, int pid)
+  {
+    //logger.debug("ParametersSimulator.systime(): inicio");
+    String sysTime = new SimpleDateFormat(format).format(new Date());
+    writeOnFile(filename, sysTime);
+    //logger.debug("ParametersSimulator.systime(): fin");
+    return sysTime;
+  }
+
+  public String secuence(String filename, String step, String length, String nonusage1, int pid)
+  {
+    //logger.debug("ParametersSimulator.secuence(): inicio");
+    int currentValue = 0;
+    String formatValue = "";
+    try
+    {
+      //Si el archivo no existe se creara uno nuevo
+      File existFile = new File(filename);
+      existFile.createNewFile();
+
+      //Se accede al archivo creado,
+      RandomAccessFile file = new RandomAccessFile(filename, "rw");
+      FileChannel fileChannel = file.getChannel();
+      FileLock lock = null;
+
+      //Pondra en cola a cualquier hilo o proceso que intente acceder al archivo.
+      lock = CheckFileLocked(lock, fileChannel);
+
+      String current = file.readLine();
+      if (!StringUtils.isNoneEmpty(current))
+      {
+        current = "0";
+      }
+      currentValue = Integer.parseInt(current);
+      currentValue += Integer.parseInt(step);
+      formatValue = StringUtils.leftPad(("" + currentValue), Integer.parseInt(length), "0");
+      file.seek(0);
+      file.writeBytes(formatValue);
+
+      Thread.sleep(500);
+      try
+      {
+        //libera el archivo para ser usado por cualquier otro proceso
+        lock.release();
+      }
+      catch (Exception ex)
+      {
+      }
+
+      file.close();
+      fileChannel.close();
     }
-    
+    catch (Exception ex)
+    {
+      throw new Error(ex);
+    }
+    //logger.debug("ParametersSimulator.secuence(): fin");
+    return formatValue;
+  }
+
+  /*Manteniendo en espera a cualquier proceso que quiera acceder al archivo bloqueado hasta que pueda acceder*/
+  private FileLock CheckFileLocked(FileLock fileLock, FileChannel fileChanel)
+  {
+    while (fileLock == null)
+    {
+      try
+      {
+        fileLock = fileChanel.tryLock();
+      }
+      catch (final OverlappingFileLockException | IOException e)
+      {
+      }
+    }
+    return fileLock;
+  }
+
+  public String reader(String filename, String format, String length, String nonusage1, int pid)
+  {
+    //logger.debug("ParametersSimulator.reader(): inicio");
+    String currentValue = "";
+    try
+    {
+
+      File file = new File(filename);
+      if (!file.exists())
+      {
+        return "";
+      }
+      String current = readFile(filename);
+      if ("N".equalsIgnoreCase(format.trim()))
+      {
+        currentValue = StringUtils.leftPad(current, Integer.parseInt(length), "0");
+      }
+      else
+      {
+        currentValue = current;
+      }
+    }
+    catch (Exception ex)
+    {
+      throw new Error(ex);
+    }
+    //logger.debug("ParametersSimulator.reader(): fin");
+    return "" + currentValue;
+  }
+
+  /**
+   * ****************************************************************************************************
+   */
+  private boolean writeOnFile(String filename, String content)
+  {
+    //logger.debug("ParametersSimulator.writeOnFile(): inicio");
+    boolean success = false;
+    try
+    {
+      File fileNew = new File(filename);
+      fileNew.createNewFile();
+
+      RandomAccessFile file = new RandomAccessFile(filename, "rw");
+      FileChannel fileChannel = file.getChannel();
+      FileLock lock = null;
+      //Pondra en cola a cualquier hilo o proceso que intente acceder al archivo.
+      lock = CheckFileLocked(lock, fileChannel);
+
+      file.seek(0);
+      file.writeBytes(content);
+
+      Thread.sleep(500);
+      try
+      {
+        //libera el archivo para ser usado por cualquier otro proceso
+        lock.release();
+      }
+      catch (Exception ex)
+      {
+      }
+      file.close();
+      fileChannel.close();
+      success = true;
+    }
+    catch (Exception ex)
+    {
+      throw new Error(ex);
+    }
+    //logger.debug("ParametersSimulator.writeOnFile(): fin");
+    return success;
+  }
+
+  private String readFile(String filename)
+  {
+    //logger.debug("ParametersSimulator.readFile(): inicio");
+    File file = new File(filename);
+    StringBuilder sb = new StringBuilder();
+    BufferedReader br;
+    try
+    {
+      file.createNewFile();
+      br = new BufferedReader(new FileReader(file));
+      String str;
+      while ((str = br.readLine()) != null)
+      {
+        System.out.println(str);
+        sb.append(str);
+      }
+    }
+    catch (Exception ex)
+    {
+      sb.append("0");
+    }
+    //logger.debug("ParametersSimulator.readFile(): fin");
+    return sb.toString();
+  }
     
 }
